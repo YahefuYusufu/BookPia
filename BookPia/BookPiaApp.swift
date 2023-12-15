17eacce83696b6b212ff91080688dfdffc69322a
YahefuYusufu <parguzatt@gmail.com>
2023-12-15 09:41:18 +0100

    Initial Commit

---
+++ BookPia/BookPiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookPiaApp.swift
+//  BookPia
+//
+//  Created by yusufyakuf on 2023-12-15.
+//
+
+import SwiftUI
+
+@main
+struct BookPiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
